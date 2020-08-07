0f5a145e2280fe70ef1161dd5be1fafecd0ac017
empfff <epfaff@email.unc.edu>
2020-08-07 14:37:24 -0400

    add new manifest table field

--- ExtractScripts/N3C_extract_pcornet_mssql.sql
+++ ExtractScripts/N3C_extract_pcornet_mssql.sql
@@ -17,10 +17,11 @@ select
    '@cdmVersion' as CDM_VERSION,
    null AS VOCABULARY_VERSION, -- hardwired null for pcornet
    '@n3cPhenotypeYN' as N3C_PHENOTYPE_YN,
-   '@n3cPhenotypeVersion' as N3C_PHENOTYPE_VERSION,
+   phenotype_version as N3C_PHENOTYPE_VERSION,
    CAST(GETDATE() as date) as RUN_DATE,
    CAST( DATEADD(day, -@dataLatencyNumDays, GETDATE()) as date) as UPDATE_DATE,	--change integer based on your site's data latency
-   CAST( DATEADD(day, @daysBetweenSubmissions, GETDATE()) as date) as NEXT_SUBMISSION_DATE;
+   CAST( DATEADD(day, @daysBetweenSubmissions, GETDATE()) as date) as NEXT_SUBMISSION_DATE
+FROM @resultsDatabaseSchema.N3C_COHORT;
 
 -- pcornet duplicate key validation script
 -- VALIDATION_SCRIPT
