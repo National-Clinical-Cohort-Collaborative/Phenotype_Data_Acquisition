9e6f7b77cd6656683d99ffa0e15e46c78f0773f6
mim18 <mim18@pitt.edu>
2020-08-11 11:40:12 -0400

    pull version from cohort table

--- ExtractScripts/N3C_extract_act_oracle.sql
+++ ExtractScripts/N3C_extract_act_oracle.sql
@@ -20,7 +20,7 @@
 
 --MANIFEST TABLE: CHANGE PER YOUR SITE'S SPECS
 --OUTPUT_FILE: MANIFEST.csv
-SELECT '@siteAbbrev' as SITE_ABBREV,
+SELECT distinct '@siteAbbrev' as SITE_ABBREV,
    '@siteName'    AS SITE_NAME,
    '@contactName' as CONTACT_NAME,
    '@contactEmail' as CONTACT_EMAIL,
@@ -28,7 +28,7 @@ SELECT '@siteAbbrev' as SITE_ABBREV,
    '@cdmVersion' as CDM_VERSION,
    null AS VOCABULARY_VERSION, -- hardwired null for ACT
    '@n3cPhenotypeYN' as N3C_PHENOTYPE_YN,
-   '@n3cPhenotypeVersion' as N3C_PHENOTYPE_VERSION,
+   phenotype_version  as N3C_PHENOTYPE_VERSION,
    CAST(SYSDATE as date) as RUN_DATE,
    CAST( (SYSDATE + NUMTODSINTERVAL(-@dataLatencyNumDays, 'day')) as date) as UPDATE_DATE,	--change integer based on your site's data latency
    CAST( (SYSDATE + NUMTODSINTERVAL(@daysBetweenSubmissions, 'day')) as date) as NEXT_SUBMISSION_DATE FROM DUAL;
