f347f431b8b660d13af02db031fe4dcd05cd2513
mim18 <mim18@pitt.edu>
2020-08-18 16:55:53 -0400

    Merged Path based and Hard Coded concept queries
    This is untested

--- PhenotypeScripts/N3C_phenotype_act_mssql.sql
+++ PhenotypeScripts/N3C_phenotype_act_mssql.sql
@@ -21,6 +21,7 @@
 --Added V2.1 LOINCs 8/11/2020
 --Removed procedures
 --Added phenotype version for manifest table
+--Merge path based with hard coded concepts
 
 
 --DROP TABLE IF EXISTS @resultsDatabaseSchema.n3c_cohort; 
@@ -86,11 +87,11 @@ with covid_loinc as
 	select 'LOINC:94647-5' as loinc UNION
 	select 'LOINC:94660-8' as loinc UNION
 	select 'LOINC:94661-6' as loinc UNION
-    	select 'UMLS:C1611271' as loinc UNION --ACT_COVID ontology terms
-    	select 'UMLS:C1335447' as loinc UNION
-    	select 'UMLS:C1334932' as loinc UNION
-    	select 'UMLS:C1334932' as loinc UNION
-    	select 'UMLS:C1335447' as loinc UNION
+    select 'UMLS:C1611271' as loinc UNION --ACT_COVID ontology terms
+    select 'UMLS:C1335447' as loinc UNION
+    select 'UMLS:C1334932' as loinc UNION
+    select 'UMLS:C1334932' as loinc UNION
+    select 'UMLS:C1335447' as loinc UNION
 	select 'LOINC:94720-0' as loinc UNION
 	select 'LOINC:94758-0' as loinc UNION
 	select 'LOINC:94759-8' as loinc UNION
@@ -120,21 +121,61 @@ with covid_loinc as
 	SELECT 'LOINC:95410-7' AS LOINC UNION 
 	SELECT 'LOINC:95411-5' AS LOINC UNION
 	-- new for v2.1
-	select '95416-4' as loinc UNION    
-	select '95424-8' as loinc UNION    
-	select '95425-5' as loinc UNION    
-	select '95427-1' as loinc UNION    
-	select '95428-9' as loinc UNION    
-	select '95429-7' as loinc UNION    
-	select '95521-1' as loinc UNION    
-	select '95522-9' as loinc 
+	select 'LOINC:95416-4' as loinc UNION    
+	select 'LOINC:95424-8' as loinc UNION    
+	select 'LOINC:95425-5' as loinc UNION    
+	select 'LOINC:95427-1' as loinc UNION    
+	select 'LOINC:95428-9' as loinc UNION    
+	select 'LOINC:95429-7' as loinc UNION    
+	select 'LOINC:95521-1' as loinc UNION    
+	select 'LOINC:95522-9' as loinc UNION
+    
+    --Path based
+    SELECT CONCEPT_CD as loinc FROM CONCEPT_DIMENSION 
+        WHERE (CONCEPT_PATH LIKE '\ACT\UMLS_C0031437\SNOMED_3947185011\UMLS_C0022885\%' 
+              AND CONCEPT_PATH NOT LIKE '\ACT\UMLS_C0031437\SNOMED_3947185011\UMLS_C0022885\UMLS_C0086143\%') 
 ),
 -- Diagnosis ICD-10 codes from phenotype doc
-covid_icd10 as
+early_dx as 
 (
+SELECT 'ICD10CM:B97.21' as icd10_code, '1_strong_positive' as dx_category FROM DUAL  
+UNION SELECT 'ICD10CM:B97.29' as icd10_code, '1_strong_positive' as dx_category FROM DUAL 
+UNION SELECT CONCEPT_CD as icd10_code, '1_strong_positive' as dx_category FROM CONCEPT_DIMENSION  
+        WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18905737\A18908307\A17799167\A17824942\A17850346\'
+UNION SELECT CONCEPT_CD  icd10_code, '1_strong_positive' as dx_category FROM CONCEPT_DIMENSION  
+        WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18905737\A18908307\A17799167\A17824942\A17773639\'
+),
+covid_dx_path as
+(
+SELECT icd10_code, dx_category from early_dx
+UNION SELECT CONCEPT_CD as dx_code, 'asymptomatic' as dx_category  FROM CONCEPT_DIMENSION WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916190\A18918864\A17785940\A17837149\A17837150\'
+UNION SELECT CONCEPT_CD as icd10_code,	'1_strong_positive' as dx_category  FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18905737\A18908307\A17799167\A17824942\A17850346\'
+UNION SELECT CONCEPT_CD  icd10_code,	'1_strong_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18905737\A18908307\A17799167\A17824942\A17773639\'
+UNION SELECT CONCEPT_CD  icd10_code,	'1_strong_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\UMLS_C0031437\SNOMED_3947185011\UMLS_C0037088\SNOMED_3947183016\ICD10CM_U07.1\'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916190\A27153708\A17837157\A17849894\A17811595\A17862489\'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18905737\A18916203\A17786353\A17799079\'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18919275\A18924508\A17816202\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18919275\A18921837\A17867137\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18919275\A18921837\A17790457\A17867138\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18919020\A17826561\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18919020\A17788101\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18913759\A17800859\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18916350\A17775366\'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18913759\A17813746\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18924251\A17826641\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18913759\A17839331\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18924231\A17852005\A17864701\'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18924251\A17800917\A17839382\'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH LIKE '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18916341\A18919034\A17800896\%'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18919275\A18906113\A17777753\A17867199\'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18919275\A18906113\A17777753\A17777754\'
+UNION SELECT CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category   FROM CONCEPT_DIMENSION  WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18919275\A18921837\A17828930\A17867142\'
+UNION select CONCEPT_CD  icd10_code,	'2_weak_positive' as dx_category  FROM CONCEPT_DIMENSION WHERE CONCEPT_PATH = '\ACT\Diagnosis\ICD10\V2_2018AA\A20098492\A18919275\A18924508\A17829004\A17777779\A17777780\'
+),
+covid_dx_coded as 
+(
+    select icd10_code, dx_category from early_dx UNION 
     select 'ICD10CM:Z11.59' as icd10_code, 'asymptomatic' as dx_category UNION
-	select 'ICD10CM:B97.21' as icd10_code,	'1_strong_positive' as dx_category UNION
-	select 'ICD10CM:B97.29' as icd10_code,	'1_strong_positive' as dx_category UNION
 	select 'ICD10CM:U07.1' as icd10_code,	'1_strong_positive' as dx_category UNION
 	select 'ICD10CM:Z20.828' as icd10_code,	'2_weak_positive' as dx_category UNION
 	select 'ICD10CM:B34.2' as icd10_code,	'2_weak_positive' as dx_category UNION
@@ -156,7 +197,12 @@ covid_icd10 as
 	select 'ICD10CM:R07.1' as icd10_code,	'2_weak_positive' as dx_category UNION
 	select 'ICD10CM:R68.83' as icd10_code,	'2_weak_positive' as dx_category 
 ),
-
+covid_icd10 as 
+(
+select icd10_code, dx_category from covid_dx_coded
+union 
+select icd10_code, dx_category from covid_dx_path
+),
 -- patients with covid related lab since start_date
 -- if using i2b2 multi-fact table please substitute 'obseravation_fact' with appropriate fact view
 covid_lab as
@@ -183,7 +229,10 @@ covid_lab_pos as
     	WHERE OBSERVATION_FACT.START_DATE >= CAST('2020-01-01' as datetime) AND  
 	(OBSERVATION_FACT.CONCEPT_CD like 'LOINC:% POSITIVE'
 		OR OBSERVATION_FACT.CONCEPT_CD = 'UMLS:C1335447' 
-		OR OBSERVATION_FACT.CONCEPT_CD = 'ACT|LOCAL|LAB:ANY POSITIVE ANTIBODY TEST')
+		OR OBSERVATION_FACT.CONCEPT_CD = 'ACT|LOCAL|LAB:ANY POSITIVE ANTIBODY TEST'
+        OR OBSERVATION_FACT.CONCEPT_CD 
+            IN (SELECT CONCEPT_CD FROM CONCEPT_DIMENSION WHERE CONCEPT_PATH LIKE '\ACT\UMLS_C0031437\SNOMED_3947185011\UMLS_C0022885\UMLS_C1335447\%')
+    )
  ),
 --Option #2 if you have not mapped to the ACT COVID Ontology
 --Search TVAL_CHAR for your COVID labs for strings that mean Positive. YOu may need to increase the list in the 
@@ -208,9 +257,9 @@ covid_diagnosis as
       start_date as best_dx_date,  -- use for later queries
         -- custom dx_category for one ICD-10 code, see phenotype doc
 		case
-			when dx in ('ICD10CM:B97.29','ICD10CM:B97.21') and start_date < CAST('2020-04-01' as datetime)  then '1_strong_positive'
-			when dx in ('ICD10CM:B97.29','ICD10CM:B97.21') and start_date >= CAST('2020-04-01' as datetime) then '2_weak_positive'
-            		when dx in ('ICD10CM:Z11.59') and start_date >= CAST('2020-04-01' as datetime) then 'asymptomatic'
+			when dx in (select icd10_code from early_dx) and start_date < CAST('2020-04-01' as datetime)  then '1_strong_positive'
+			when dx in (select icd10_code from early_dx) and start_date >= CAST('2020-04-01' as datetime) then '2_weak_positive'
+            when dx in (select icd10_code from asymp_dx) and start_date >= CAST('2020-04-01' as datetime) then 'asymptomatic'
 			else dxq.orig_dx_category
 		end as dx_category        
     from
@@ -329,7 +378,7 @@ n3c_cohort as
 		left outer join dx_strong on covid_cohort.patient_num = dx_strong.patient_num
 		left outer join dx_weak on covid_cohort.patient_num = dx_weak.patient_num
 		left outer join covid_lab on covid_cohort.patient_num = covid_lab.patient_num
-        	left outer join dx_asymp on covid_cohort.patient_num = dx_asymp.patient_num
+        left outer join dx_asymp on covid_cohort.patient_num = dx_asymp.patient_num
 
 
 )
