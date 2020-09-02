724b4b2d1ccc37d687ca473c28c5507c9064a29c
LoraLingrey-TriNetX <lora.lingrey@trinetx.com>
2020-09-02 12:29:42 -0400

    TNX: Strip pipe characters from fields

--- ExtractScripts/N3C_extract_trinetx.sql
+++ ExtractScripts/N3C_extract_trinetx.sql
@@ -18,16 +18,16 @@ DROP TABLE IF EXISTS :TNX_SCHEMA.n3c_manifest;
 CREATE TABLE :TNX_SCHEMA.n3c_patient AS
 SELECT
 	n3c.patient_id	AS PATIENT_ID
-	, LEFT(pt.birth_date::varchar,7)	AS BIRTH_DATE	--only pull YYYY-MM
-	, pt.vital_status	AS VITAL_STATUS
-	, pt.death_date::datetime	AS DEATH_DATE
-	, LEFT(pt.postal_code,5)	AS POSTAL_CODE
-	, pt.gender		AS SEX
-	, pt.race		AS RACE
-	, pt.ethnicity	AS ETHNICITY
-	, pt.language	AS LANGUAGE
-	, pt.marital_status	AS MARITAL_STATUS
-	, pt.smoking_status	AS SMOKING_STATUS
+	, LEFT(pt.birth_date::varchar,7)		AS BIRTH_DATE	--only pull YYYY-MM
+	, REPLACE(pt.vital_status,'|',' ')		AS VITAL_STATUS
+	, pt.death_date::datetime				AS DEATH_DATE
+	, LEFT(pt.postal_code,5)				AS POSTAL_CODE
+	, REPLACE(pt.gender,'|',' ')			AS SEX
+	, REPLACE(pt.race,'|',' ')				AS RACE
+	, REPLACE(pt.ethnicity,'|',' ')			AS ETHNICITY
+	, REPLACE(pt.language,'|',' ')			AS LANGUAGE
+	, REPLACE(pt.marital_status,'|',' ')	AS MARITAL_STATUS
+	, REPLACE(pt.smoking_status,'|',' ')	AS SMOKING_STATUS
 	, map_sx.mt_code	AS MAPPED_SEX
 	, map_rc.mt_code	AS MAPPED_RACE
 	, map_et.mt_code	AS MAPPED_ETHNICITY
@@ -46,14 +46,14 @@ FROM :TNX_SCHEMA.n3c_cohort n3c
 ---------------------------------------------------------------------------------------------------------
 CREATE TABLE :TNX_SCHEMA.n3c_encounter AS
 SELECT
-	n3c.patient_id	AS PATIENT_ID
-	, enc.encounter_id	AS ENCOUNTER_ID
-	, enc.type	AS ENCOUNTER_TYPE
-	, enc.start_date	AS START_DATE
-	, enc.end_date		AS END_DATE
-	, enc.length_of_stay	AS LENGTH_OF_STAY
-	, enc.orphan	AS ORPHAN_FLAG
-	, map_et.mt_code	AS MAPPED_ENCOUNTER_TYPE
+	n3c.patient_id				AS PATIENT_ID
+	, enc.encounter_id			AS ENCOUNTER_ID
+	, REPLACE(enc.type,'|',' ')	AS ENCOUNTER_TYPE
+	, enc.start_date			AS START_DATE
+	, enc.end_date				AS END_DATE
+	, enc.length_of_stay		AS LENGTH_OF_STAY
+	, enc.orphan				AS ORPHAN_FLAG
+	, map_et.mt_code			AS MAPPED_ENCOUNTER_TYPE
 FROM :TNX_SCHEMA.n3c_cohort n3c
 	JOIN :TNX_SCHEMA.encounter enc ON enc.patient_id = n3c.patient_id AND enc.start_date >= '2018-01-01'
 	LEFT JOIN :TNX_SCHEMA.mapping map_et ON map_et.provider_code = ('TNX:ENCOUNTER_TYPE:' || enc.type)
@@ -69,16 +69,16 @@ WHERE enc.source_id NOT IN ('Diamond','Datavant')
 ---------------------------------------------------------------------------------------------------------
 CREATE TABLE :TNX_SCHEMA.n3c_diagnosis AS
 SELECT
-	n3c.patient_id	AS PATIENT_ID
-	, dx.encounter_id	AS ENCOUNTER_ID
-	, dx.code_system	AS DX_CODE_SYSTEM
-	, dx.code	AS DX_CODE
-	, dx.date	AS DATE
-	, dx.description	AS DX_DESCRIPTION
-	, dx.principal_indicator	AS PRINCIPAL_INDICATOR
-	, dx.source	AS DX_SOURCE
-	, dx.orphan	AS ORPHAN_FLAG
-	, dx.orphan_reason	AS ORPHAN_REASON
+	n3c.patient_id						AS PATIENT_ID
+	, dx.encounter_id					AS ENCOUNTER_ID
+	, dx.code_system					AS DX_CODE_SYSTEM
+	, dx.code							AS DX_CODE
+	, dx.date							AS DATE
+	, REPLACE(dx.description,'|',' ')	AS DX_DESCRIPTION
+	, dx.principal_indicator			AS PRINCIPAL_INDICATOR
+	, dx.source							AS DX_SOURCE
+	, dx.orphan							AS ORPHAN_FLAG
+	, dx.orphan_reason					AS ORPHAN_REASON
 	, SPLIT_PART(map_dx.mt_code,':',2)	AS MAPPED_CODE_SYSTEM
 	, SPLIT_PART(map_dx.mt_code,':',3)	AS MAPPED_CODE
 FROM :TNX_SCHEMA.n3c_cohort n3c
@@ -96,14 +96,14 @@ WHERE dx.source_id NOT IN ('Diamond','Datavant')
 ---------------------------------------------------------------------------------------------------------
 CREATE TABLE :TNX_SCHEMA.n3c_procedure AS
 SELECT
-	n3c.patient_id	AS PATIENT_ID
-	, px.encounter_id	AS ENCOUNTER_ID
-	, px.code_system	AS PX_CODE_SYSTEM
-	, px.code	AS PX_CODE
-	, px.description	AS PX_DESCRIPTION
-	, px.date	AS DATE
-	, px.orphan	AS ORPHAN_FLAG
-	, px.orphan_reason	AS ORPHAN_REASON
+	n3c.patient_id						AS PATIENT_ID
+	, px.encounter_id					AS ENCOUNTER_ID
+	, px.code_system					AS PX_CODE_SYSTEM
+	, px.code							AS PX_CODE
+	, REPLACE(px.description,'|',' ')	AS PX_DESCRIPTION
+	, px.date							AS DATE
+	, px.orphan							AS ORPHAN_FLAG
+	, px.orphan_reason					AS ORPHAN_REASON
 	, CASE
 		WHEN REGEXP_COUNT(map_px.mt_code,':') = 2 THEN SPLIT_PART(map_px.mt_code,':',2)
 		WHEN REGEXP_COUNT(map_px.mt_code,':') = 1 THEN SPLIT_PART(map_px.mt_code,':',1)
@@ -113,7 +113,7 @@ SELECT
 		WHEN REGEXP_COUNT(map_px.mt_code,':') = 2 THEN SPLIT_PART(map_px.mt_code,':',3)
 		WHEN REGEXP_COUNT(map_px.mt_code,':') = 1 THEN SPLIT_PART(map_px.mt_code,':',2)
 		ELSE map_px.mt_code
-		END	AS MAPPED_CODE
+		END								AS MAPPED_CODE
 FROM :TNX_SCHEMA.n3c_cohort n3c
 	JOIN :TNX_SCHEMA.procedure px ON px.patient_id = n3c.patient_id AND px.date >= '2018-01-01'
 	LEFT JOIN :TNX_SCHEMA.mapping map_px ON map_px.provider_code = (px.code_system || ':' || px.code)
@@ -129,34 +129,34 @@ WHERE px.source_id NOT IN ('Diamond','Datavant')
 ---------------------------------------------------------------------------------------------------------
 CREATE TABLE :TNX_SCHEMA.n3c_medication AS
 SELECT
-	n3c.patient_id	AS PATIENT_ID
-	, rx.encounter_id	AS ENCOUNTER_ID
-	, rx.code_system	AS RX_CODE_SYSTEM
-	, rx.code	AS RX_CODE
-	, rx.name	AS RX_DESCRIPTION
-	, rx.alt_drug_code_sys	AS ALT_DRUG_CODE_SYS
-	, rx.alt_drug_code	AS ALT_DRUG_CODE
-	, rx.start_date::datetime	AS START_DATE
-	, rx.route_of_administration	AS ROUTE_OF_ADMINISTRATION
-	, rx.units_per_administration	AS UNITS_PER_ADMINISTRATION
-	, rx.frequency	AS FREQUENCY
-	, rx.strength	AS STRENGTH
-	, rx.form	AS FORM
-	, rx.duration	AS DURATION
-	, rx.refills	AS REFILLS
-	, rx.source	AS RX_SOURCE
-	, rx.indication_code_system	AS INDICATION_CODE_SYSTEM
-	, rx.indication_code	AS INDICATION_CODE
-	, rx.indication_desc	AS INDICATION_DESC
-	, rx.alt_drug_name	AS ALT_DRUG_NAME
-	, rx.clinical_drug	AS CLINICAL_DRUG
-	, rx.end_date::datetime	AS END_DATE
-	, rx.quantity_dispensed	AS QTY_DISPENSED
-	, rx.dose_amount	AS DOSE_AMOUNT
-	, rx.dose_unit	AS DOSE_UNIT
-	, rx.brand	AS BRAND
-	, rx.orphan	AS ORPHAN_FLAG
-	, rx.orphan_reason	AS ORPHAN_REASON
+	n3c.patient_id									AS PATIENT_ID
+	, rx.encounter_id								AS ENCOUNTER_ID
+	, rx.code_system								AS RX_CODE_SYSTEM
+	, rx.code										AS RX_CODE
+	, REPLACE(rx.name,'|',' ')						AS RX_DESCRIPTION
+	, rx.alt_drug_code_sys							AS ALT_DRUG_CODE_SYS
+	, rx.alt_drug_code								AS ALT_DRUG_CODE
+	, rx.start_date::datetime						AS START_DATE
+	, rx.route_of_administration					AS ROUTE_OF_ADMINISTRATION
+	, REPLACE(rx.units_per_administration,'|',' ')	AS UNITS_PER_ADMINISTRATION
+	, REPLACE(rx.frequency,'|',' ')					AS FREQUENCY
+	, REPLACE(rx.strength,'|',' ')					AS STRENGTH
+	, REPLACE(rx.form,'|',' ')						AS FORM
+	, rx.duration									AS DURATION
+	, rx.refills									AS REFILLS
+	, rx.source										AS RX_SOURCE
+	, rx.indication_code_system						AS INDICATION_CODE_SYSTEM
+	, rx.indication_code							AS INDICATION_CODE
+	, REPLACE(rx.indication_desc,'|',' ')			AS INDICATION_DESC
+	, REPLACE(rx.alt_drug_name,'|',' ')				AS ALT_DRUG_NAME
+	, rx.clinical_drug								AS CLINICAL_DRUG
+	, rx.end_date::datetime							AS END_DATE
+	, rx.quantity_dispensed							AS QTY_DISPENSED
+	, rx.dose_amount								AS DOSE_AMOUNT
+	, rx.dose_unit									AS DOSE_UNIT
+	, REPLACE(rx.brand,'|',' ')						AS BRAND
+	, rx.orphan										AS ORPHAN_FLAG
+	, rx.orphan_reason								AS ORPHAN_REASON
 	, CASE
 		WHEN REGEXP_COUNT(map_rx.mt_code,':') = 2 THEN SPLIT_PART(map_rx.mt_code,':',2)
 		WHEN REGEXP_COUNT(map_rx.mt_code,':') = 1 THEN SPLIT_PART(map_rx.mt_code,':',1)
@@ -166,7 +166,7 @@ SELECT
 		WHEN REGEXP_COUNT(map_rx.mt_code,':') = 2 THEN SPLIT_PART(map_rx.mt_code,':',3)
 		WHEN REGEXP_COUNT(map_rx.mt_code,':') = 1 THEN SPLIT_PART(map_rx.mt_code,':',2)
 		ELSE map_rx.mt_code
-		END	AS MAPPED_CODE
+		END											AS MAPPED_CODE
 FROM :TNX_SCHEMA.n3c_cohort n3c
 	JOIN :TNX_SCHEMA.medication rx ON rx.patient_id = n3c.patient_id AND rx.start_date >= '2018-01-01'
 	LEFT JOIN :TNX_SCHEMA.mapping map_rx ON map_rx.provider_code = (rx.code_system || ':' || rx.code)
@@ -182,26 +182,26 @@ WHERE rx.source_id NOT IN ('Diamond','Datavant')
 ---------------------------------------------------------------------------------------------------------
 CREATE TABLE :TNX_SCHEMA.n3c_lab_result AS
 SELECT
-	n3c.patient_id	AS PATIENT_ID
-	, lab.encounter_id	AS ENCOUNTER_ID
-	, lab.observation_code_system	AS LAB_CODE_SYSTEM
-	, lab.observation_code	AS LAB_CODE
-	, lab.observation_desc	AS LAB_DESCRIPTION
-	, lab.battery_code_system	AS BATTERY_CODE_SYSTEM
-	, lab.battery_code	AS BATTERY_CODE
-	, lab.battery_desc	AS BATTERY_DESC
-	, lab.section	AS SECTION
-	, lab.normal_range	AS NORMAL_RANGE
-	, lab.test_date	AS TEST_DATE
-	, lab.result_type	AS RESULT_TYPE
-	, lab.lab_result_num_val	AS NUMERIC_RESULT_VAL
-	, lab.lab_result_text_val	AS TEXT_RESULT_VAL
-	, lab.units_of_measure	AS UNITS_OF_MEASURE
-	, lab.orphan	AS ORPHAN_FLAG
-	, lab.orphan_reason	AS ORPHAN_REASON
-	, SPLIT_PART(map_lab.mt_code,':',2)	AS MAPPED_CODE_SYSTEM
-	, SPLIT_PART(map_lab.mt_code,':',3)	AS MAPPED_CODE
-	, SPLIT_PART(map_res.mt_code,':',3)	AS MAPPED_TEXT_RESULT_VAL
+	n3c.patient_id								AS PATIENT_ID
+	, lab.encounter_id							AS ENCOUNTER_ID
+	, lab.observation_code_system				AS LAB_CODE_SYSTEM
+	, lab.observation_code						AS LAB_CODE
+	, REPLACE(lab.observation_desc,'|',' ')		AS LAB_DESCRIPTION
+	, lab.battery_code_system					AS BATTERY_CODE_SYSTEM
+	, lab.battery_code							AS BATTERY_CODE
+	, REPLACE(lab.battery_desc,'|',' ')			AS BATTERY_DESC
+	, lab.section								AS SECTION
+	, lab.normal_range							AS NORMAL_RANGE
+	, lab.test_date								AS TEST_DATE
+	, lab.result_type							AS RESULT_TYPE
+	, lab.lab_result_num_val					AS NUMERIC_RESULT_VAL
+	, REPLACE(lab.lab_result_text_val,'|',' ')	AS TEXT_RESULT_VAL
+	, lab.units_of_measure						AS UNITS_OF_MEASURE
+	, lab.orphan								AS ORPHAN_FLAG
+	, lab.orphan_reason							AS ORPHAN_REASON
+	, SPLIT_PART(map_lab.mt_code,':',2)			AS MAPPED_CODE_SYSTEM
+	, SPLIT_PART(map_lab.mt_code,':',3)			AS MAPPED_CODE
+	, SPLIT_PART(map_res.mt_code,':',3)			AS MAPPED_TEXT_RESULT_VAL
 FROM :TNX_SCHEMA.n3c_cohort n3c
 	JOIN :TNX_SCHEMA.lab_result lab ON lab.patient_id = n3c.patient_id AND lab.test_date >= '2018-01-01'
 	LEFT JOIN :TNX_SCHEMA.mapping map_lab ON map_lab.provider_code = (lab.observation_code_system || ':' || lab.observation_code)
@@ -218,18 +218,18 @@ WHERE lab.source_id NOT IN ('Diamond','Datavant')
 ---------------------------------------------------------------------------------------------------------
 CREATE TABLE :TNX_SCHEMA.n3c_vital_signs AS
 SELECT
-	n3c.patient_id	AS PATIENT_ID
-	, vit.encounter_id	AS ENCOUNTER_ID
-	, vit.measure_date	AS MEASURE_DATE
-	, vit.code_system	AS VITAL_CODE_SYSTEM
-	, vit.code	AS VITAL_CODE
-	, vit.description	AS VITAL_DESCRIPTION
-	, vit.unit_of_measure	AS UNIT_OF_MEASURE
-	, vit.result_type	AS RESULT_TYPE
-	, vit.numeric_value	AS NUMERIC_RESULT_VAL
-	, vit.text_value	AS TEXT_RESULT_VAL
-	, vit.orphan	AS ORPHAN_FLAG
-	, vit.orphan_reason	AS ORPHAN_REASON
+	n3c.patient_id						AS PATIENT_ID
+	, vit.encounter_id					AS ENCOUNTER_ID
+	, vit.measure_date					AS MEASURE_DATE
+	, vit.code_system					AS VITAL_CODE_SYSTEM
+	, vit.code							AS VITAL_CODE
+	, REPLACE(vit.description,'|',' ')	AS VITAL_DESCRIPTION
+	, vit.unit_of_measure				AS UNIT_OF_MEASURE
+	, vit.result_type					AS RESULT_TYPE
+	, vit.numeric_value					AS NUMERIC_RESULT_VAL
+	, REPLACE(vit.text_value,'|',' ')	AS TEXT_RESULT_VAL
+	, vit.orphan						AS ORPHAN_FLAG
+	, vit.orphan_reason					AS ORPHAN_REASON
 	, SPLIT_PART(map_vit.mt_code,':',2)	AS MAPPED_CODE_SYSTEM
 	, SPLIT_PART(map_vit.mt_code,':',3)	AS MAPPED_CODE
 	, SPLIT_PART(map_res.mt_code,':',3)	AS MAPPED_TEXT_RESULT_VAL
@@ -264,17 +264,17 @@ COMMIT;
 ---------------------------------------------------------------------------------------------------------
 CREATE TABLE :TNX_SCHEMA.n3c_manifest AS
 SELECT
-	:MNFST_SITE_ABBREV	AS SITE_ABBREV
-	, :MNFST_SITE_NAME	AS SITE_NAME
-	, :MNFST_CONTACT_NAME	AS CONTACT_NAME
-	, :MNFST_CONTACT_EMAIL	AS CONTACT_EMAIL
-	, 'TRINETX'	AS CDM_NAME
-	, ''	AS CDM_VERSION
-	, ''	AS VOCABULARY_VERSION
-	, 'Y'	AS N3C_PHENOTYPE_YN
-	, phenoVers.version	AS N3C_PHENOTYPE_VERSION
+	:MNFST_SITE_ABBREV					AS SITE_ABBREV
+	, :MNFST_SITE_NAME					AS SITE_NAME
+	, :MNFST_CONTACT_NAME				AS CONTACT_NAME
+	, :MNFST_CONTACT_EMAIL				AS CONTACT_EMAIL
+	, 'TRINETX'							AS CDM_NAME
+	, ''								AS CDM_VERSION
+	, ''								AS VOCABULARY_VERSION
+	, 'Y'								AS N3C_PHENOTYPE_YN
+	, phenoVers.version					AS N3C_PHENOTYPE_VERSION
 	, CURRENT_TIMESTAMP(0)::datetime	AS RUN_DATE
-	, MAX(import_date)	AS UPDATE_DATE
+	, MAX(import_date)					AS UPDATE_DATE
 	, TIMESTAMPADD(DAY, :MNFST_SUBMISSION_OFFSET, CURRENT_TIMESTAMP(0))::datetime	AS NEXT_SUBMISSION_DATE
 FROM :LOG_SCHEMA.audit
 LEFT JOIN :TNX_SCHEMA.n3c_pheno_version phenoVers on 1=1
