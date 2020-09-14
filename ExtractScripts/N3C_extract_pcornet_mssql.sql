3f1d06344873c0f243e5449c1d4f290799f5ffab
empfff <epfaff@email.unc.edu>
2020-09-14 12:37:01 -0400

    Update N3C_extract_pcornet_mssql.sql

--- ExtractScripts/N3C_extract_pcornet_mssql.sql
+++ ExtractScripts/N3C_extract_pcornet_mssql.sql
@@ -18,8 +18,8 @@ select distinct
    null AS VOCABULARY_VERSION, -- hardwired null for pcornet
    '@n3cPhenotypeYN' as N3C_PHENOTYPE_YN,
    phenotype_version as N3C_PHENOTYPE_VERSION,
-   '@shiftDateYN' as SHIFT_DATE_YN,
-   '@maxNumShiftDays' as MAX_NUM_SHIFT_DAYS,
+   '@shiftDateYN' as SHIFT_DATE_YN, --if shifting dates prior to submission say Y, else N
+   '@maxNumShiftDays' as MAX_NUM_SHIFT_DAYS, --maximum number of days that you are shifting dates, write UNKNOWN if you do not know, NA if not shifting
    CAST(GETDATE() as date) as RUN_DATE,
    CAST( DATEADD(day, -@dataLatencyNumDays, GETDATE()) as date) as UPDATE_DATE,	--change integer based on your site's data latency
    CAST( DATEADD(day, @daysBetweenSubmissions, GETDATE()) as date) as NEXT_SUBMISSION_DATE
