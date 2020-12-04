86dbbc81c2b10d4e270823e4041eb315240a0fb7
empfff <epfaff@email.unc.edu>
2020-12-04 09:46:59 -0500

    added schema name - thanks wake!

--- PhenotypeScripts/N3C_phenotype_pcornet_oracle.sql
+++ PhenotypeScripts/N3C_phenotype_pcornet_oracle.sql
@@ -609,7 +609,7 @@ INSERT INTO @resultsDatabaseSchema.N3C_FINAL_MAP (case_patid,control_patid,buddy
 	demog2.sex as control_sex,
 	demog2.race as control_race,
 	demog2.hispanic as control_ethn
-FROM N3C_PENULTIMATE_MAP penultimate_map
+FROM @resultsDatabaseSchema.N3C_PENULTIMATE_MAP penultimate_map
 	join @cdmDatabaseSchema.demographic demog1 on penultimate_map.patid = demog1.patid
 	left outer join @cdmDatabaseSchema.demographic demog2 on penultimate_map.control_patid = demog2.patid
  )
