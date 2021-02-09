31181d3cce96a2bf1283b7cc77da212296cf1083
empfff <epfaff@email.unc.edu>
2021-02-09 12:35:01 -0500

    Update N3C_phenotype_omop_oracle.sql

--- PhenotypeScripts/N3C_phenotype_omop_oracle.sql
+++ PhenotypeScripts/N3C_phenotype_omop_oracle.sql
@@ -781,7 +781,7 @@ AS (SELECT covid_cohort.person_id
 	,inc_dx_weak
 	,inc_lab_any
 	,inc_lab_pos
-	,'3.0' AS phenotype_version
+	,'3.1' AS phenotype_version
 	,CASE
 		WHEN floor(months_between(SYSDATE, d.birth_datetime)/12) BETWEEN 0
 				AND 4
