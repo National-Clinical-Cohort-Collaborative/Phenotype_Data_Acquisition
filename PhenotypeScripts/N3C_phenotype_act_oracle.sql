022d78cc07ea54a75566cf432bb88c635e824329
empfff <epfaff@email.unc.edu>
2021-02-09 12:33:06 -0500

    Update N3C_phenotype_act_oracle.sql

--- PhenotypeScripts/N3C_phenotype_act_oracle.sql
+++ PhenotypeScripts/N3C_phenotype_act_oracle.sql
@@ -390,7 +390,7 @@ SELECT DISTINCT
     inc_dx_weak, 
     inc_lab_any, 
     inc_lab_pos, 
-    '3.0' AS phenotype_version,
+    '3.1' AS phenotype_version,
     case when d.current_age between 0 and 4 then '0-4'
         when d.current_age between 5 and 9 then '5-9'
         when d.current_age between 10 and 14 then '10-14'
