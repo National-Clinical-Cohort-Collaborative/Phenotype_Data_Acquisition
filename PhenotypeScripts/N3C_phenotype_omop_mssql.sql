db5e70ae5c44578237e038b825f2b0f3fadc59b1
empfff <epfaff@email.unc.edu>
2021-06-21 12:17:11 -0400

    Update N3C_phenotype_omop_mssql.sql

--- PhenotypeScripts/N3C_phenotype_omop_mssql.sql
+++ PhenotypeScripts/N3C_phenotype_omop_mssql.sql
@@ -844,7 +844,7 @@ SELECT DISTINCT c.person_id
 	,inc_dx_weak
 	,inc_lab_any
 	,inc_lab_pos
-	,'3.2' AS phenotype_version
+	,'3.3' AS phenotype_version
 	,CASE
 		WHEN datediff(year, d.birth_datetime, getdate()) BETWEEN 0
 				AND 4
